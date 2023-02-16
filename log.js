600057aff2f221c3f06b3d9c8426b9141629ec3c
Sachin M Sibichan <sachinm06@aol.com>
2023-02-16 15:19:41 +0530

    first commit

---
+++ log.js
@@ -0,0 +1,17 @@
+function login(){
+
+uname=username.value
+
+localStorage.setItem("uname",uname)
+
+    window.location="home.html"
+}
+
+usern=localStorage.getItem("uname")
+
+d1.innerHTML=usern
+
+function logout(){
+    window.location="index.html"
+    localStorage.removeItem("usern")
+}
